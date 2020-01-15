f8203083eb310ab4c348f8c8e7c65e4ef9b1997d
sarathethiraj <https://github.com/sarathethiraj>
2020-01-15 21:53:01 +0530

    setup class chaged

--- src/main/java/sampleTest/setup.java
+++ src/main/java/sampleTest/setup.java
@@ -10,8 +10,12 @@ public class setup {
 	@BeforeMethod
 	public void setup() {
 		String s=System.getProperty("user.dir");
+		System.out.println("--------------------------");
+		System.out.println("Browser going to launched");
 		System.setProperty("webdriver.chrome.driver", s+"\\driver\\chromedriver.exe");
 		dr = new ChromeDriver();
+		System.out.println("--------------------------");
+		System.out.println("Browser is launched");
 		dr.get("http://www.leafground.com/");
 		
 	}
