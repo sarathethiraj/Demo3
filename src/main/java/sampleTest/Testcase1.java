36d9f2d8e5a4851edea8a640869e5ac21f7962a5
sarathethiraj <https://github.com/sarathethiraj>
2020-01-18 09:30:11 +0530

    changed the browser to desired capabilities

--- src/main/java/sampleTest/Testcase1.java
+++ src/main/java/sampleTest/Testcase1.java
@@ -1,5 +1,8 @@
 package sampleTest;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,6 +16,8 @@ public class Testcase1 extends setup {
 
 		
 		dr.findElement(By.xpath("//*[@id=\"post-153\"]/div[2]/div/ul/li[1]/a")).click();
+		//assert.assertTrue(dr.getTitle(), "TestLeaf - Interact with Edit Fields");
+		//assert.assertEquals(dr.getTitle(), "TestLeaf - Interact with Edit Fields");
 		System.out.println("Testcase1 -passed");
 
 	}
--- src/main/java/sampleTest/setup.java
+++ src/main/java/sampleTest/setup.java
@@ -2,6 +2,7 @@ package sampleTest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeMethod;
 
 public class setup {
@@ -12,8 +13,10 @@ public class setup {
 		String s=System.getProperty("user.dir");
 		System.out.println("--------------------------");
 		System.out.println("Browser going to launched");
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("chrome");
 		System.setProperty("webdriver.chrome.driver", s+"\\driver\\chromedriver.exe");
-		dr = new ChromeDriver();
+		dr = new ChromeDriver(caps);
 		System.out.println("--------------------------");
 		System.out.println("Browser is launched");
 		dr.get("http://www.leafground.com/");
